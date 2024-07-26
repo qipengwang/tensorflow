f77dfbe60ca1729b301dce03e610028770fb4896
qipengwang <qipengwang@tencent.com>
2024-07-26 12:01:21 +0800

    update 53

--- tensorflow/core/common_runtime/gpu/gpu_process_state.cc
+++ tensorflow/core/common_runtime/gpu/gpu_process_state.cc
@@ -196,17 +196,15 @@ Allocator* GPUProcessState::GetGPUAllocator(
     //     options.experimental().internal_fragmentation_fraction());
     // Allocator* gpu_allocator = gpu_bfc_allocator;
     Allocator* gpu_allocator = nullptr;
-    GPUBFCAllocator* gpu_bfc_allocator = 
-        new GPUBFCAllocator(sub_allocator, total_bytes, options,
-                            strings::StrCat("GPU_", tf_device_id.value(), "_bfc"),
-                            options.experimental().internal_fragmentation_fraction());;
     if (useTensorPoolAllocator()) {
       gpu_allocator =
-          new GPUTensorPoolAllocator(sub_allocator, gpu_bfc_allocator,
+          new GPUTensorPoolAllocator(sub_allocator,
                       strings::StrCat("GPU_", tf_device_id.value(), "_tensorpool"),
                       total_bytes);
     } else {
-      gpu_allocator = gpu_bfc_allocator;
+      gpu_allocator = new GPUBFCAllocator(sub_allocator, total_bytes, options,
+                                          strings::StrCat("GPU_", tf_device_id.value(), "_bfc"),
+                                          options.experimental().internal_fragmentation_fraction());;;
     }
 
     SharedCounter* timing_counter = nullptr;
